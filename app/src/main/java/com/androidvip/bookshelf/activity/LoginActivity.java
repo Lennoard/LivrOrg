4168de9d5653114c5e5585359d97ed18281818c1
lennoard <lennoardrai@gmail.com>
2018-02-05 18:06:12 -0300

    Lab¹

---
+++ app/src/main/java/com/androidvip/bookshelf/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.androidvip.bookshelf.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidvip.bookshelf.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
